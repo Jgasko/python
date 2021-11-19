d21ba7a4bf6b891fcf5564c0c3e509cdb2aaebf3
Jgasko <jgasko@gmail.com>
2021-11-19 09:05:40 -0500

    initial commit

---
+++ hello world.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+#make sure everything is working
+print ("Hello World!")
+
+#take user input and display it
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+print ("Hello " + firstName + " " + lastName)
+
+#for loop
+arr = [1,2,3,4,5,6,7,8,9]
+for x in arr:
+    print (x)
+
+#add data to an array and display it afterwards
+number = int(input("How many items are you adding to the list? "))
+names = []
+for i in range(0,number):
+    name = input("Enter a name: ")
+    names.append(name)
+print(names)
+
