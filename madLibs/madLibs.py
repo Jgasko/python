b3cdc6fee9e26f75c923d1fd2a5612981da26073
Jgasko <jgasko@gmail.com>
2021-11-19 16:08:19 -0500

    initial code

---
+++ madLibs/madLibs.py
@@ -0,0 +1,19 @@
+#madlibs generator
+
+#get inputs
+print ("Hint: a Verb is an action. A noun is a person/place/thing. An adjective describes a person/place/thing.")
+noun1 = input("Enter a noun: ")
+p_noun1 = input("Enter a plural noun: ")
+noun2 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+verb2 = input("Enter a verb: ")
+verb3 = input("Enter a verb: ")
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter an adjective: ")
+
+print ("Be kind to your " + noun1 + "-footed " + p_noun1)
+print ("For a duck may be somebody's " + noun2)
+print ("Be kind to your " + p_noun1 + " in " + verb1)
+print ("Where the weater is always " + verb2)
+print ("You may think that this is the " + verb3)
+print ("Well, it is!")
